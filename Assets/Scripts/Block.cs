293dd682ba5989066f781813a73030dee9778850
Hampus Liljekvist <hlilje@users.noreply.github.com>
2020-04-24 15:55:23 +0200

    Initial commit

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    void Start() {
+        Debug.Log("Block");
+    }
+
+    void Update() {
+    }
+
+    void OnCollisionEnter(Collision collider) {
+        Debug.Log("Collision");
+
+        if (collider.gameObject.tag == "Block") {
+            // TODO: Attach with spring
+            // TODO: Notify block spawner to increase height
+            Debug.Log("Block hit!");
+        } else if (collider.gameObject.tag == "Ground") {
+            // TODO: Attach to ground
+            // TODO: Notify block spawner to increase height
+            Debug.Log("Ground hit!");
+        } else {
+            // ASSERT
+        }
+    }
+}
---
+++ Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+    public GameObject prefab;
+
+    private int blockHeight = 1; // TODO: Base off mesh size
+    private int sideLimits = 2;
+    private int direction = 1;
+    private float Speed = 2;
+
+    void Start() {
+        Debug.Log("BlockSpawner");
+
+        GameObject parent = Instantiate(prefab, transform.position, Quaternion.identity);
+        parent.transform.parent = transform;
+        parent.GetComponent<Rigidbody>().isKinematic = true;
+
+        // TODO: Pre-spawn initial block
+    }
+
+    void Update() {
+        UpdatePosition();
+
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            SpawnBlock();
+        }
+    }
+
+    void UpdatePosition() {
+        Vector3 position = transform.position;
+        position.x += direction * (Speed * Time.deltaTime);
+        if (position.x <= -sideLimits || position.x >= sideLimits)
+        {
+            position.x = position.x < 0 ? -sideLimits : sideLimits;
+            direction *= -1;
+        }
+        transform.position = position;
+    }
+
+    void SpawnBlock() {
+        Instantiate(prefab, transform.position, Quaternion.identity);
+
+        Vector3 position = transform.position;
+        position.y += blockHeight;
+        transform.position = position;
+
+        GameObject camera = GameObject.Find("Main Camera");
+        Vector3 cameraPos = camera.transform.position;
+        cameraPos.y += blockHeight;
+        camera.transform.position = cameraPos;
+    }
+}
