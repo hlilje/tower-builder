00241eede288cfa395c0d9eb1b507cbc0c8cc489
Hampus Liljekvist <hlilje@users.noreply.github.com>
2020-05-29 10:48:45 +0200

    Make is possible to adjust camera height

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private float groundOffset = 0.2f;
     private float speed = 10.0f;
 
-    void Update()
+    private void Update()
     {
         Vector3 position = transform.position;
         float delta = Time.deltaTime * speed;
 
-        if (Input.GetKey("up")) {
-            position.z += delta;
-        } else if (Input.GetKey("down")) {
-            position.z -= delta;
+        if (Input.GetKey("left shift")) {
+            if (Input.GetKey("up")) {
+                position.y += delta;
+            } else if (Input.GetKey("down")) {
+                GameObject ground = GameObject.Find("Ground");
+                position.y -= delta;
+                float limit = ground.transform.position.y + groundOffset;
+                position.y = Mathf.Max(position.y, groundOffset);
+            }
+        } else {
+            if (Input.GetKey("up")) {
+                position.z += delta;
+            } else if (Input.GetKey("down")) {
+                position.z -= delta;
+            }
         }
 
         transform.position = position;
