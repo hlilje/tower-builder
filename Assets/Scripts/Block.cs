8be1ddf6cfc6a6e0a89d77d8dca7b5d134942064
Hampus Liljekvist <hlilje@users.noreply.github.com>
2020-05-29 17:15:30 +0200

    Attach blocks with spring joints

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,7 +5,9 @@ public class Block : MonoBehaviour {
     private ContactPoint[] _contactPoints = new ContactPoint[_maxContactCount];
 
     private void OnCollisionEnter(Collision collision) {
-        Debug.Log("Collision");
+        if (GetComponent<Rigidbody>().isKinematic) {
+            return;
+        }
 
         int contacts = collision.GetContacts(_contactPoints);
         if (contacts == 0) {
@@ -21,17 +23,19 @@ public class Block : MonoBehaviour {
 
             // TODO: Notify block spawner to increase height
 
-            //if (!gameObject.GetComponent<SpringJoint>()) {
-            //    Debug.Log("Attached with SpringJoint");
+            // TODO: This will add two springs for each collision
+            if (!gameObject.GetComponent<Rigidbody>().isKinematic) {
+                Debug.Log("Attached to block with SpringJoint");
 
-            //    gameObject.AddComponent<SpringJoint>();
-            //    gameObject.GetComponent<SpringJoint>().connectedBody = collision.rigidbody;
-            //}
+                gameObject.AddComponent<SpringJoint>();
+                gameObject.GetComponent<SpringJoint>().connectedBody = GetComponent<Rigidbody>();
+            }
         } else if (gameObject.tag == Object.ground) {
             Debug.Log("Ground hit!");
 
+            // Only the first block is attached to the ground
             if (!gameObject.GetComponent<FixedJoint>()) {
-                Debug.Log("Attached with FixedJoint");
+                Debug.Log("Attached to ground with FixedJoint");
 
                 gameObject.AddComponent<FixedJoint>();
                 gameObject.GetComponent<FixedJoint>().connectedBody = GetComponent<Rigidbody>();
