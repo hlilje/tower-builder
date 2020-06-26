63bbab3ec87dca22d635fc42ee9f12b040e8648e
Hampus Liljekvist <hlilje@users.noreply.github.com>
2020-06-26 13:24:08 +0200

    Scale speed with distance

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-    private const float _speed = 10.0f;
+    private const float _baseMoveSpeed = 10.0f;
+    private const float _rotationSpeed = 150.0f;
+    private const float _distanceSpeedFactor = 0.1f;
+
     private const float _groundOffset = 0.2f;
 
     private Vector3 _position;
     private Vector3 _blockSpawnerPosition;
+
     private bool _free = false;
 
     private void Start() {
@@ -28,36 +32,48 @@ public class CameraController : MonoBehaviour {
     }
 
     private void UpdatePosition() {
-        Vector3 position = transform.position;
-        float delta = Time.deltaTime * _speed;
+        float moveSpeed = CalcMoveSpeed();
+        float moveDelta = Time.deltaTime * moveSpeed;
+        float rotationDelta = Time.deltaTime * _rotationSpeed;
 
         if (Input.GetKey(Key.cameraRotateLeft)) {
-            transform.RotateAround(_blockSpawnerPosition, Vector3.up, _speed * delta);
+            transform.RotateAround(_blockSpawnerPosition, Vector3.up, rotationDelta);
         } else if (Input.GetKey(Key.cameraRotateRight)) {
-            transform.RotateAround(_blockSpawnerPosition, Vector3.up, -_speed * delta);
+            transform.RotateAround(_blockSpawnerPosition, Vector3.up, -rotationDelta);
         } else {
+            Vector3 position = transform.position;
+
             if (Input.GetKey(Key.cameraModifier)) {
                 if (Input.GetKey(Key.cameraUp)) {
-                    position.y += delta;
+                    position.y += moveDelta;
                 } else if (Input.GetKey(Key.cameraDown)) {
                     GameObject ground = GameObject.Find(Object.ground);
-                    position.y -= delta;
+                    position.y -= moveDelta;
                     float limit = ground.transform.position.y + _groundOffset;
                     position.y = Mathf.Max(position.y, _groundOffset);
                 }
             } else {
-                Vector3 targetPos = _blockSpawnerPosition;
-                targetPos.y = position.y;
-                Vector3 deltaVec = (position - targetPos).normalized * delta;
+                Vector3 moveDeltaVec = CalcDirectionVector(position, true) * moveDelta;
                 if (Input.GetKey(Key.cameraForward)) {
-                    position -= deltaVec;
+                    position -= moveDeltaVec;
                 } else if (Input.GetKey(Key.cameraBack)) {
-                    position += deltaVec;
+                    position += moveDeltaVec;
                 }
             }
 
             transform.position = position;
         }
+    }
+
+    private float CalcMoveSpeed() {
+        float distance = CalcDirectionVector(transform.position, false).magnitude;
+        return Mathf.Max(_baseMoveSpeed, _baseMoveSpeed * distance * _distanceSpeedFactor);
+    }
+
+    private Vector3 CalcDirectionVector(Vector3 position, bool normalise) {
+        Vector3 targetPos = _blockSpawnerPosition;
+        targetPos.y = position.y;
+        return normalise ? (position - targetPos).normalized : (position - targetPos);
     }
 
     public void OnBlockSpawned(float blockHeight) {
